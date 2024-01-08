b1d9696e117b23cae2600b63e33075bf0963288a
jihoLee <wlgh0324@gmail.com>
2024-01-08 14:07:51 +0900

    urls 경로 설정

--- ExcelCalculate/ExcelCalculate/urls.py
+++ ExcelCalculate/ExcelCalculate/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('email/', include('sendEmail.urls'), name='eamil'),
+    path('calculate/', include('calculate.urls'), name='calculate'),
+    path('', include('main.urls'), name='main'),
     path('admin/', admin.site.urls),
+
 ]
